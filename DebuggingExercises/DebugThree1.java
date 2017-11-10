eb157cd8e21963dc87d67b7091a5373c5d4dd51d
gareth,ullins <gareth.mullins@my.jcu.edu.au>
2017-11-10 15:49:58 +1000

    First commit.

--- DebuggingExercises/DebugThree1.java
+++ DebuggingExercises/DebugThree1.java
@@ -1,18 +1,21 @@
 // This class calculates a waitperson's tip as 15% of the bill
 public class DebugThree1
 {
-   public static void main(String args[])
-   {
-      double myCheck = 50.00;
-      double yourCheck = 19.95;
-      System.out.println("Tips are");
-      calcTip(mycheck);
-      calctip(yourCheck);
-    }
-    public static int calcTip(int bill)
+    public static void main(String args[])
     {
-      final double RATE = 0.15;
-      tip = bill + RATE;
-      System.out.println("The tip should be at least " + tip);
+        double myCheck = 50.00;
+        double yourCheck = 19.95;
+
+        System.out.println("Tips are");
+
+        calcTip(myCheck);
+        calcTip(yourCheck);
+    }
+    private static void calcTip(double bill)
+    {
+        final double RATE = 0.15;
+        double tip = bill + RATE;
+        System.out.printf("The tip should be at least %.2f", tip);
+        System.out.println();
     }
 }
--- DebuggingExercises/DebugThree3.java
+++ DebuggingExercises/DebugThree3.java
@@ -2,22 +2,24 @@
 import java.util.Scanner;
 public class DebugThree3
 {
-   public static void main(String args[])
-   {
-      String name;
-      getName();
-      displayGreeting(name);           
-   }
-   public static String getName()
-   {
-      String name;
-      Scanner input = new Scanner(System.in);
-      System.out.print(Enter name );
-      name = input.nextInt();
-      return name;
-   }
-   public static void displayGreeting()
-   {
-      System.out.println("Hello, " + name + "!");
-   }
+    public static void main(String args[])
+    {
+        String name = getName();
+
+        displayGreeting(name);
+    }
+    private static String getName()
+    {
+        String name;
+
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter name ");
+        name = input.nextLine();
+
+        return name;
+    }
+    private static void displayGreeting(String name)
+    {
+        System.out.println("Hello, " + name + "!");
+    }
 }
